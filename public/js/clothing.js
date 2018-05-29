2604be491dd685000f3136d704035763c1ea0704
Weipeng <13546782483@163.com>
2018-05-29 15:21:18 +0800

    重新做

---
+++ public/js/clothing.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+    var row=document.getElementsByClassName("row");
+    var liArr=row.document.getElementsByClassName("li");
+    //没有实现
+    for (var i=0;i<liArr.length;i++) {
+        alert("nihao");
+        this.style.opacity="0.5";
+        // liArr[i].onmouseover=function(){
+        //     for(var j=0;j<liArr.length;j++){
+        //         liArr[j].style.opacity="";
+        //     }
+        //     this.style.opacity="0.5";
+        // }
+    }
+
+}
